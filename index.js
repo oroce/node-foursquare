aaea07b6c19171b811fa6d2f5e0e4feca2df574b
oroce <robert@oroszi.net>
2012-11-17 20:08:48 +0100

    photo api v1

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+/*jshint camelcase:true curly:true eqeqeq:true noempty:true quotmark:double undef:true expr:true node:true es5:true*/
+
+"use strict";
+
 var modules = {
+	Core: require( "./lib/core" ),
 	Venue: require( "./lib/venue" )
 };
 module.exports = function( config ){
@@ -7,7 +12,7 @@ module.exports = function( config ){
 	}
 	config.v = config.v||(function(){
 		var d = new Date();
-		return d.getFullYear()+( "0"+ (d.getMonth()+1) ).slice(-1)+( "0"+ d.getDate() ).slice(-1);
+		return d.getFullYear()+( "0"+ (d.getMonth()+1) ).slice(-2)+( "0"+ d.getDate() ).slice(-2);
 	})();
 	Object.keys( modules ).forEach(function( m ){
 		modules[ m ].config = config;
